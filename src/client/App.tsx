36982c909399c3305c885466158862127f7a00df
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-10-28 10:27:55 -0500

    Building up Chirp component, need to finish then pair up with BrowserRouter

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from './components/navigation/Navbar'
+import Chirp from './components/common/Chirp';
+import Moment from 'react-moment';
+import moment from 'moment';
+
 
 
 
@@ -21,6 +25,7 @@ class App extends React.Component<IAppProps, IAppState> {
         return (
 			<Container>
 				<Navbar></Navbar>
+                <button onClick={() => <Chirp user="me" chirpId="0z" body="hello" time={<Moment date={moment()}/>}/>}>Chorp</button>
 	            {/* <input value={this.state.text} onChange={this.onInputChange} /> */}
 			</Container>
         );
--- src/client/components/common/Chirp.tsx
+++ src/client/components/common/Chirp.tsx
@@ -1,1 +1,28 @@
 import * as React from 'react';
+import Moment from 'react-moment';
+import moment from 'moment';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent(Swal);
+const chirpModal = MySwal.fire({
+    title: "Login/Signup",
+    html: `<p>Hey</p>`,
+    footer: "Copyright Chirper 2020",
+    focusConfirm: true
+});
+
+const Chirp: React.FC<ChirpProps> = () => {
+    return (
+        (chirpModal as any)
+    );
+}
+
+export interface ChirpProps {
+    user: string,
+    chirpId: string,
+    time: any,
+    body: string    
+ };
+
+export default Chirp;
