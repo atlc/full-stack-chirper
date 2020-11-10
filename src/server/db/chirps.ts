d9a8160539cdefcc62a70b938a9ef95b752c5cf8
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-11-10 17:12:04 -0600

    Basic CRUD operations writing to DB successfully

--- src/server/db/chirps.ts
+++ src/server/db/chirps.ts
@@ -3,9 +3,14 @@ import { Query } from './index';
 const all = async () => Query('SELECT * from Chirps');
 const chirp_by_id = async (id: number) => Query('SELECT * from Chirps where id = ?', [id]);
 const new_chirp = async (userId: number, content: string, location: string) => Query('INSERT into Chirps (userid, content, location) VALUES (?, ?, ?)', [userId, content, location]);
+const update_chirp = async (id: number, content: string) => Query('UPDATE Chirps SET content = ? where id = ?', [content, id]);
+const delete_chirp = async (id: number) => Query('DELETE from Chirps where id = ?', [id]);
+
 
 export default {
     all,
     chirp_by_id,
-    new_chirp
+    new_chirp,
+    update_chirp,
+    delete_chirp
 }
--- src/server/routes/chirps.ts
+++ src/server/routes/chirps.ts
@@ -11,7 +11,7 @@ router.get('/:id?', async (req, res) => {
         id ? res.json(await db.Chirps.chirp_by_id(id)) : res.json(await db.Chirps.all());
     } catch(e) {
         console.log(e);
-        res.send('Uh oh, we made a fucky-wucky').sendStatus(500);
+        res.send('A server error has occurred.').sendStatus(500);
     }    
 });
 
@@ -23,32 +23,31 @@ router.post('/', async (req, res) => {
         res.json(await db.Chirps.new_chirp(userId, content, location));
     } catch(e) {
         console.log(e);
-        res.send('Uh oh, we made a fucky-wucky').sendStatus(500);
+        res.send('A server error has occurred.').sendStatus(500);
     }
 });
 
 
-// router.put('/:id?', (req, res) => {
-//     let id: number = +req.params.id;
-//     let putDTO = {...req.body};
-//     let updatedAt: number = Date.now();
-//     if (id || id === 0) {
-//         // chirpsStore.UpdateChirp(id, {updatedAt, ...putDTO});
-//         res.send('Updated Chirp!').sendStatus(200);
-//     } else {
-//         res.send('Could not find chirp with that id.').sendStatus(404);
-//     }
-// });
+router.put('/:id?', async (req, res) => {
+    try {
+        let id: number = +req.params.id;
+        let content: string = req.body.content;
+        id ? res.json(await db.Chirps.update_chirp(id, content)) : res.sendStatus(404);
+    } catch(e) {
+        console.log(e);
+        res.send('A server error has occurred.').sendStatus(500);
+    }    
+});
 
-// router.delete('/:id?', (req, res) => {
-//     let id: number = +req.params.id;
-//     if (id || id === 0) {
-//         // chirpsStore.DeleteChirp(id);
-//         res.send('Destroyed chirp!').sendStatus(200);
-//     } else {
-//         res.sendStatus(404).send('Chirp not located.');
-//     }
-// });
+router.delete('/:id?', async (req, res) => {
+    try {
+        let id: number = +req.params.id;
+        id ? res.json(await db.Chirps.delete_chirp(id)) : res.sendStatus(404);
+    } catch(e) {
+        console.log(e);
+        res.send('A server error has occurred.').sendStatus(500);
+    }    
+});
 
 
 export default router;
