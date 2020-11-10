eb04ce734bff7170a8ef61bb3b4d42073c2c98bf
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-11-10 16:31:57 -0600

    Got POST, GET, and GET by ID working

--- src/server/db/chirps.ts
+++ src/server/db/chirps.ts
@@ -1,7 +1,11 @@
 import { Query } from './index';
 
 const all = async () => Query('SELECT * from Chirps');
+const chirp_by_id = async (id: number) => Query('SELECT * from Chirps where id = ?', [id]);
+const new_chirp = async (userId: number, content: string, location: string) => Query('INSERT into Chirps (userid, content, location) VALUES (?, ?, ?)', [userId, content, location]);
 
 export default {
-    all
+    all,
+    chirp_by_id,
+    new_chirp
 }
--- src/server/routes/chirps.ts
+++ src/server/routes/chirps.ts
@@ -1,46 +1,54 @@
 import * as express from 'express';
+import db from '../db';
 
-let router = express.Router();
+const router = express.Router();
+const app = express();
+app.use(express.json());
 
-router.get('/:id?', (req, res) => {
-    let id: number = +req.params.id;
-    res.send('hey');
-    // (id || id === 0) ? res.json(chirpsStore.GetChirp(id)).sendStatus(200) : res.send(chirpsStore.GetChirps()).sendStatus(200);
+router.get('/:id?', async (req, res) => {
+    try {
+        let id: number = +req.params.id;
+        id ? res.json(await db.Chirps.chirp_by_id(id)) : res.json(await db.Chirps.all());
+    } catch(e) {
+        console.log(e);
+        res.send('Uh oh, we made a fucky-wucky').sendStatus(500);
+    }    
 });
 
-router.post('/', (req, res) => {
-    let postDTO = {...req.body};
-    let createdAt: number = Date.now();
-    // chirpsStore.CreateChirp({createdAt, ...postDTO});
-    res.send('Posted Chirp!').sendStatus(201);
-});
-
-router.put('/:id?', (req, res) => {
-    let id: number = +req.params.id;
-    let putDTO = {...req.body};
-    let updatedAt: number = Date.now();
-    if (id || id === 0) {
-        // chirpsStore.UpdateChirp(id, {updatedAt, ...putDTO});
-        res.send('Updated Chirp!').sendStatus(200);
-    } else {
-        res.send('Could not find chirp with that id.').sendStatus(404);
+router.post('/', async (req, res) => {
+    try {
+        let userId: number = +req.body.userId;
+        let content: string = req.body.content;
+        let location: string = req.body.location;
+        res.json(await db.Chirps.new_chirp(userId, content, location));
+    } catch(e) {
+        console.log(e);
+        res.send('Uh oh, we made a fucky-wucky').sendStatus(500);
     }
 });
 
-router.delete('/:id?', (req, res) => {
-    let id: number = +req.params.id;
-    if (id || id === 0) {
-        // chirpsStore.DeleteChirp(id);
-        res.send('Destroyed chirp!').sendStatus(200);
-    } else {
-        res.sendStatus(404).send('Chirp not located.');
-    }
-});
+
+// router.put('/:id?', (req, res) => {
+//     let id: number = +req.params.id;
+//     let putDTO = {...req.body};
+//     let updatedAt: number = Date.now();
+//     if (id || id === 0) {
+//         // chirpsStore.UpdateChirp(id, {updatedAt, ...putDTO});
+//         res.send('Updated Chirp!').sendStatus(200);
+//     } else {
+//         res.send('Could not find chirp with that id.').sendStatus(404);
+//     }
+// });
+
+// router.delete('/:id?', (req, res) => {
+//     let id: number = +req.params.id;
+//     if (id || id === 0) {
+//         // chirpsStore.DeleteChirp(id);
+//         res.send('Destroyed chirp!').sendStatus(200);
+//     } else {
+//         res.sendStatus(404).send('Chirp not located.');
+//     }
+// });
 
 
 export default router;
-
-
-
-// Add DTO written at per router request
-// Add res.json responses per requesroot
